7867861df278848c2274e4ccb2e1fe87f15bfcd6
DessertProfessor <reneverheij@users.noreply.github.com>
2020-03-13 15:01:09 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
+import socket
+import datetime
+
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!\n"
+    hostname = socket.gethostname()
+    host_ip = socket.gethostbyname(hostname)
+    currentDT = datetime.datetime.now()
+    currentDT_formatted = currentDT.strftime("%Y-%m-%d %H:%M:%S")
+    return = currentDT_formatted + ": Hello, World! This is a message from host: " + hostname + " (" + host_ip + ")\n"
 
 @application.route("/ready")
 def ready():
